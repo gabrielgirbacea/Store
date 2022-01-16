8e1edafd51e8f1e705420a4f744d84ad09feb9da
Gabriel Girbacea <gabriel.girbacea@gmail.com>
2022-01-16 09:02:35 +0100

    Added logic to the client to retrieve the list of products. Added self signed certificate

--- Store.Api/wwwroot/client/ng-store/src/app/app.component.ts
+++ Store.Api/wwwroot/client/ng-store/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ng-store';
+export class AppComponent implements OnInit {
+  title = 'Store Title';
+  products: IProduct[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<IPagination>('https://localhost:5001/api/products?pageSize=50').subscribe(
+      (response: IPagination ) => {
+        this.products = response!.data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
